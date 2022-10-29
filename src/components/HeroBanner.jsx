24e28d0121f28285f159e91c4d4352e1b9348ce7
mohit-04 <mohitrana04032003@gmail.com>
2022-10-29 13:39:27 +0530

    Hero Banner- kawai

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/woman.jpg";
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
-  )
-}
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        my: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Walk-in Fitness
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: {
+            lg: "44px",
+            sx: "40px",
+          },
+        }}
+        mb="23px"
+        mt="30px"
+      >
+        You are condemned <br /> To REPEAT
+      </Typography>
+      <Typography fontSize="22px" lineHieght="35px" mb={4}>
+        Follow for more
+      </Typography>
 
-export default HeroBanner
+      <Button
+        variant="contained"
+        color="error"
+        href="#excercise"
+        sx={{
+          backgroundColor: "#ff2625",
+          padding: "10px",
+        }}
+      >
+        Explore Exercise
+      </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{
+          opacity: 0.1,
+          display: { lg: "block", xs: "none" },
+        }}
+        fontSize="200px"
+      >
+        {" "}
+        FLEXXXX!!!{" "}
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+    </Box>
+  );
+};
+
+export default HeroBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
       <HeroBanner />
       <SearchExercises />
       <Exercises />
-
      </Box>
   )
 }
