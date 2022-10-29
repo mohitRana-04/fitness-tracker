40dda1071213f5fc54a637d3b61360caf97b4831
mohit-04 <mohitrana04032003@gmail.com>
2022-10-29 10:46:48 +0530

    file structure complete

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import {Route, Routes} from 'react-router-dom'
 import {Box} from '@mui/material'
 
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
-        <Box width="400px">
+        {/* xl - extra large device  m - margin*/}
+        <Box width="400px" sx={{ width: {xl:'1488px'} }} m="auto" >
             <Navbar/>
             <Routes>
                 <Route path='/' element={<Home />}/>
                 {/* this id is dynamic */}
                 <Route path="/exercise/:id" element={<ExerciseDetail />}/>
             </Routes>
+            <Footer/>
         </Box>
     </div>
   )
